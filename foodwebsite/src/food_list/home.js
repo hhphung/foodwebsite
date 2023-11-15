69364f4751e36dead51ffe22a39e91f98d219632
hoi phung <hhphung@iastate.edu>
2023-11-15 11:39:17 -0600

    random recipes layout

--- foodwebsite/src/food_list/home.js
+++ foodwebsite/src/food_list/home.js
@@ -1,11 +1,45 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import './home.css'; // Import your custom CSS file
 
 function Home() {
-  const foodImage = 'https://spoonacular.com/recipeImages/654959-312x231.jpg';
+  const recipesSets = [
+    [
+      {
+        id: 1,
+        title: 'Recipe 1',
+        description: 'Some quick example text for Recipe 1.',
+        image: 'https://spoonacular.com/recipeImages/654959-312x231.jpg',
+      },
+      {
+        id: 2,
+        title: 'Recipe 2',
+        description: 'Some quick example text for Recipe 2.',
+        image: 'https://spoonacular.com/recipeImages/123456-312x231.jpg',
+      },
+      // Add more recipe data as needed for the first set
+    ],
+    [
+      {
+        id: 3,
+        title: 'Recipe 3',
+        description: 'Some quick example text for Recipe 3.',
+        image: 'https://spoonacular.com/recipeImages/789012-312x231.jpg',
+      },
+      {
+        id: 4,
+        title: 'Recipe 4',
+        description: 'Some quick example text for Recipe 4.',
+        image: 'https://spoonacular.com/recipeImages/345678-312x231.jpg',
+      },
+      // Add more recipe data as needed for the second set
+    ],
+    // Add more sets of recipes as needed
+  ];
 
   return (
     <>
@@ -15,17 +49,23 @@ function Home() {
         </em>
       </header>
       <div className="p-3 mb-2 bg-light text-dark">
-        <Container className="list-random">
-          <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={foodImage} />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of the card's content.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Container>
+        {recipesSets.map((recipes, setIndex) => (
+          <Container key={setIndex} className="list-random">
+            <Row>
+              {recipes.map((recipe) => (
+                <Col key={recipe.id}>
+                  <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={recipe.image} alt={recipe.title} />
+                    <Card.Body>
+                      <Card.Title>{recipe.title}</Card.Title>
+                      <Card.Text>{recipe.description}</Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              ))}
+            </Row>
+          </Container>
+        ))}
       </div>
     </>
   );
